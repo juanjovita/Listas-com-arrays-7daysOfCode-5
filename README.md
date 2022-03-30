2d7f6095752ebff155c10e242551d3769d0bb154
juanjovita <juanjovita22@gmail.com>
2022-03-30 13:22:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-w4ks1x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-w4ks1x)
---
+++ index.js
@@ -0,0 +1,6 @@
+//Lista de compras 
+
+let frutas = []
+frutas.push(prompt('escolha uma fruta'))
+
+window.document.write(frutas)
